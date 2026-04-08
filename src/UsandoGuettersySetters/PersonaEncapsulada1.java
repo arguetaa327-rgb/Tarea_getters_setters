1edf0d7416532eac63a21a8f49d848c64f952464
Alondra Argueta <arguetaa327@gmail.com>
2026-04-07 22:08:04 -0600

    Se realizo el ejercicio1

--- src/UsandoGuettersySetters/PersonaEncapsulada1.java
+++ src/UsandoGuettersySetters/PersonaEncapsulada1.java
@@ -2,9 +2,37 @@ package UsandoGuettersySetters;
 
 public class PersonaEncapsulada1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	    private String nombre;
+	    private int edad;
+	    private String genero;
 
+	    public String getNombre() {
+	        return nombre;
+	    }
+
+	    public void setNombre(String nombre) {
+	        this.nombre = nombre;
+	    }
+
+	    public int getEdad() {
+	        return edad;
+	    }
+
+	    public void setEdad(int edad) {
+	        if (edad >= 0) {
+	            this.edad = edad;
+	        } else {
+	            this.edad = 0;
+	            System.out.println("La edad no puede ser negativa. Se asigno 0.");
+	        }
+	    }
+
+	    public String getGenero() {
+	        return genero;
+	    }
+
+	    public void setGenero(String genero) {
+	        this.genero = genero;
 	}
 
 }
