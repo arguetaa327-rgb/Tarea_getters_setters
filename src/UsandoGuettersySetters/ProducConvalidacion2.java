007a6aeceba9a1898ea3b46a8bd4d5d5040bc7f5
Alondra Argueta <arguetaa327@gmail.com>
2026-04-07 22:11:22 -0600

    Se termino de realizar el ejer 2

--- src/UsandoGuettersySetters/ProducConvalidacion2.java
+++ src/UsandoGuettersySetters/ProducConvalidacion2.java
@@ -2,9 +2,63 @@ package UsandoGuettersySetters;
 
 public class ProducConvalidacion2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	    private String codigo;
+	    private String nombre;
+	    private double precio;
+	    private int stock;
 
+	    public String getCodigo() {
+	        return codigo;
+	    }
+
+	    public void setCodigo(String codigo) {
+	        this.codigo = codigo;
+	    }
+
+	    public String getNombre() {
+	        return nombre;
+	    }
+
+	    public void setNombre(String nombre) {
+	        this.nombre = nombre;
+	    }
+
+	    public double getPrecio() {
+	        return precio;
+	    }
+
+	    public void setPrecio(double precio) {
+	        if (precio > 0) {
+	            this.precio = precio;
+	        } else {
+	            System.out.println("El precio debe ser mayor a 0.");
+	        }
+	    }
+
+	    public int getStock() {
+	        return stock;
+	    }
+
+	    public void setStock(int stock) {
+	        if (stock >= 0) {
+	            this.stock = stock;
+	        } else {
+	            System.out.println("El stock no puede ser negativo.");
+	        }
+	    }
+
+	    public void vender(int cantidad) {
+	        if (cantidad > 0) {
+	            if (cantidad <= stock) {
+	                stock = stock - cantidad;
+	                System.out.println("Venta realizada. Stock restante: " + stock);
+	            } else {
+	                System.out.println("No hay suficientes unidades en stock.");
+	            }
+	        } else {
+	            System.out.println("La cantidad a vender debe ser mayor a 0.");
+	        }
+	    }
 	}
+	
 
-}
